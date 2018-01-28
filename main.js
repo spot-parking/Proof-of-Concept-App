a03358c75e05059a9c9625c5395fb34d06a69b8f
Steve Tan <i@stevetan.me>
2018-01-28 22:31:20 +0800

    rename stuff

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
-const camera = require('./lib/util/image-processing.js').camera;
+const imageProcessingUtil = require('./lib/util/image-processing.js');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
