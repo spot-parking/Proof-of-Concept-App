e8e8eef19183cf7c5c345ef5e2f4a82fb33ff1a1
Steve Tan <i@stevetan.me>
2018-02-20 23:51:23 +0800

    tweaks

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const licensePlateRecognitionUtil = require('./lib/util/license-plate-recognition');
+const path = require('path');
 
 licensePlateRecognitionUtil
     .start()
     .then(() => {
         return licensePlateRecognitionUtil
-            .processImage(`/Users/stevetan/Downloads/lp.jpg`)
+            .processImage(path.join(__dirname, `lp-1.jpg`));
     })
     .then(result => {
-        console.log(`RESULT: ${JSON.stringify(result, null, 4)}`);
+        console.log(`RESULT 1: ${JSON.stringify(result, null, 4)}`);
+
+        return licensePlateRecognitionUtil
+            .processImage(path.join(__dirname, `lp-2.jpg`));
+    })
+    .then(result => {
+        console.log(`RESULT 2: ${JSON.stringify(result, null, 4)}`);
 
         return licensePlateRecognitionUtil.stop();
     })
